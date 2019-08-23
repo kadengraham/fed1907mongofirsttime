fb3a1257266517eeeb05afc12e5e7cf0026dc88f
Kaden Graham <kaden.graham@sixconsultingcorp.com>
2019-08-23 10:37:27 -0400

    Changed insert method

--- mongodb-connect.js
+++ mongodb-connect.js
@@ -9,17 +9,9 @@ MongoClient.connect('mongodb://localhost:27017', (err, client) => {
     }
 
     console.log('Connected to MongoDB Server!!');
-    
+
     db.collection('FirstMongo').insertOne({
         taskName: 'Do something else',
-        completed: false
-    }, (err, result) => {
-        if(err){
-            console.log('There was something wrong when inserting data!');
-        }
-    })
-    db.collection('FirstMongo').insertOne({
-        taskName: 'Get it done',
         completed: false
     }, (err, result) => {
         if(err){
